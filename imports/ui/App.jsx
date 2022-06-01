c4b713d9a471122b5fde1c62f71b6dde91fcb880
José Vitor <josevitorguigarc@gmail.com>
2022-06-01 14:30:59 -0300

    Primeiras alterações

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from "react";
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <h1>Meteor Wallet</h1>
+    <form>
+      <div>
+        <label htmlFor="name">Nome:</label>
+        <input id="name" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" />
+      </div>
+      <div>
+        <label htmlFor="imageUrl">Imagem de Perfil:</label>
+        <input id="imageUrl" type="text" />
+      </div>
+      <div>
+        <button>Salvar Contato</button>
+      </div>
+    </form>
   </div>
 );
