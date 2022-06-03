ee49aacdc9f730609329dce5370db27527444d30
José Vitor <josevitorguigarc@gmail.com>
2022-06-03 15:40:12 -0300

    alterações no css

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { ContactForm } from "./ContactForm";
 import { ContactList } from "./ContactList";
+import { Header } from "./Header";
 export const App = () => (
   <div>
-    <h1>Meteor Wallet</h1>
-   
-   <ContactForm></ContactForm>
-   <ContactList></ContactList>
+    <Header></Header>
+    <div className="min-h-full">
+      <div className="max-w-4xl mx-auto p-2">
+        <ContactForm></ContactForm>
+        <ContactList></ContactList>
+      </div>
+    </div>
   </div>
 );
