d2932a4646334a5dd35aed861a83f75714b3d260
José Vitor <josevitorguigarc@gmail.com>
2022-06-03 20:54:36 -0300

    ALTERAÇÕES METODOS

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { ContactForm } from "./ContactForm";
-import { ContactList } from "./ContactList";
-import { Header } from "./Header";
+import React from 'react';
+import {ContactForm} from "./ContactForm";
+import {ContactList} from "./ContactList";
+import {Header} from "./Header";
+
 export const App = () => (
   <div>
-    <Header></Header>
+    <Header />
     <div className="min-h-full">
       <div className="max-w-4xl mx-auto p-2">
-        <ContactForm></ContactForm>
-        <ContactList></ContactList>
+        <ContactForm />
+        <ContactList />
       </div>
     </div>
   </div>
